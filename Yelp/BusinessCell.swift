16b647591f4b6695b1633fb8f26d78200b48b989
vvenkatavijay <Vijay@Venkatas-MacBook-Pro.local>
2015-05-17 15:22:07 -0700

    basic app

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,13 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Venkata Vijay on 5/15/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import Cocoa
+
+class BusinessCell: UITableViewCell {
+
+}
---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,13 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by Venkata Vijay on 5/17/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import Cocoa
+
+class SwitchCell: UITableViewCell {
+
+}
